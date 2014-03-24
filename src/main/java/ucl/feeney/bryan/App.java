67e2276609ab259b1b305add2ce69c211315294a
Bryan Feeney <bryan.feeney@gmail.com>
2014-03-24 12:38:24 +0000

    Added initial Maven project

---
+++ src/main/java/ucl/feeney/bryan/App.java
@@ -0,0 +1,13 @@
+package ucl.feeney.bryan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
